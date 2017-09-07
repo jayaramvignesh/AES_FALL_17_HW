72bafe8638308381e21badad266e589c1e6c9adc
jayaramvignesh <jayaramvignesh@gmail.com>
2017-09-07 12:14:50 -0600

    Adding script which will build the kernel

---
+++ HW1/build_script/build_kernel.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import subprocess
+
+subprocess.call("sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc",shell=True,stdout=None)
+
+subprocess.call("cp /boot/config-$(uname -r) .config",shell=True,stdout=None)
+subprocess.call("make oldconfig",shell=True,stdout=None)
+
+subprocess.call("sudo make -j4 && sudo make modules_install -j4 && sudo make install -j4",shell=True,stdout=None)
+
+subprocess.call("sudo update-initramfs -c -k 4.12.10",shell=True,stdout=None)
+
+subprocess.call("sudo update-grub",shell=True,stdout=None)
