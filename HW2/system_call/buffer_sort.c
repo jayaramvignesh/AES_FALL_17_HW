e5b6a9275a5aee3c35e10e78a3c0cac8a30e088f
jayaramvignesh <jayaramvignesh@gmail.com>
2017-09-18 15:38:44 -0600

    adding negative tests

--- HW2/system_call/buffer_sort.c
+++ HW2/system_call/buffer_sort.c
@@ -23,57 +23,65 @@ void random_int(int32_t *unsorted,uint32_t length)
 
 int main()
 {
+  
+  uint32_t length = BUFFER_LENGTH;
 
-	uint32_t length = BUFFER_LENGTH;
-
-  /*Validate Length*/
+  /*Check if length is equal to zero*/
   if(length == 0)
-  {
-    printf("length is zero\n");
+  { 
+    printf("length is zero");
     return 0;
   }
-  
-  /*Allocating memory required to store the integers[for both before and after sorting]*/
+
   int32_t* unsorted = malloc(length*sizeof(uint32_t));
-	int32_t* sorted = malloc(length*sizeof(uint32_t));
-  
-  /*Check for NULL after malloc*/
+  int32_t* sorted = malloc(length*sizeof(uint32_t));
+ 
+  /*Check if pointers are equal to NULL*/
   if(unsorted == NULL || sorted == NULL)
   {
     printf("/nERROR:MALLOC/n");
     return 0;
   }
 
-	int i = 0;
-	long int return_value;	
+  int i = 0;
+  long int return_value;	
 	
-  /*Call function to fill the buffer with random int values*/ 
-	random_int(unsorted,length);
+  random_int(unsorted,length);
 
-	printf("\nArray before sorting \n");
-	for(i=0; i<length; i++)
-	{
-		printf("%d\n",*(unsorted+i));
-	}
+  printf("\nArray before sorting \n");
+  for(i=0; i<length; i++)
+  {
+	printf("%d\n",*(unsorted+i));
+  }
 
-
-  /*Sytem call*/
-	return_value = syscall(334,unsorted,sorted,length);
-	printf("\nreturn value is %ld\n",return_value);
+  /*Positive testing of system call*/
+  return_value = syscall(334,unsorted,sorted,length);
+  printf("\nreturn value is %ld\n",return_value);
 
 
+  printf("\nArray after sorting: \n");
+  for(i=0; i<length; i++)
+  {
+	printf("%d\n",*(sorted+i));
+  }
 
-	printf("\nArray after sorting: \n");
-	for(i=0; i<length; i++)
-	{
-		printf("%d\n",*(sorted+i));
-	}
+
+  /*Testing of function when wrong inputs are passed*/
+  int32_t *a = NULL;
+  uint32_t l = 0; 
+
+  return_value = syscall(334,a,sorted,length);
+  printf("\nWhen pointer to store unsorted numbers is NULL, return value is %ld\n",return_value);
+
+  return_value = syscall(334,unsorted,a,length);
+  printf("\nWhen pointer to store sorted numbers is NULL, return value is %ld\n",return_value);
+
+  return_value = syscall(334,unsorted,sorted,l);
+  printf("\nWhen length is 0, return value is %ld\n",return_value);
 
   /*Free the allocated memory*/
   free(unsorted);
   free(sorted);
-
-
 }
 
 
