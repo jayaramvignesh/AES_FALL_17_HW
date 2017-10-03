89c0f982fceb78bb4eba5c838a2198d239e09eb8
jayaramvignesh <jayaramvignesh@gmail.com>
2017-10-02 22:29:27 -0600

    added extra credit part to code

--- HW3/Multithreading/multithreading.c
+++ HW3/Multithreading/multithreading.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 199506L
+
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
@@ -7,7 +9,11 @@
 #include<stdint.h>
 #include<string.h>
 #include<stdbool.h>
+#include <ctype.h>
 
+#define MAX_WORD_SIZE 100
+#define MAX_WORDS 1024
+#define FREQ_CNT 5
 #define SIZE 1024
 char filename[100] = "";
 
@@ -17,7 +23,19 @@ typedef struct
   int line_cnt;
   int char_cnt;
   int word_cnt;
+  int no_top_repeated_words;
 }file_stats;
+
+/*structure for frequenct word info*/
+typedef struct words_1
+{
+  char s[MAX_WORD_SIZE];
+  int freq;
+}word_map;
+
+/*create a structure instance for the word frequency*/
+word_map words[MAX_WORDS];
+
 
 /*create a global structure instance*/
 file_stats statistics;
@@ -28,6 +46,101 @@ pthread_t th1_id, th2_id;
 FILE *fp;
 pthread_mutex_t file_lock;
 pthread_mutex_t count_lock;
+
+/*function to insert a word into structure. and increment count of word if already present*/
+void word_insert(word_map *words, int* no_of_words, char *c)
+{
+
+  /*search if word is present nad if yes increment freq*/
+  for(int i = 0; i< *no_of_words; i++)
+  {
+    if(strcmp(c,words[i].s) == 0)
+    {
+      words[i].freq++;
+      return;
+    }
+  }
+
+  /*check for error conditions*/
+  if(strlen(c) >= MAX_WORD_SIZE)
+  {
+    printf("\nWORD IS TOO LONG\n");
+    exit(1);
+  }
+
+  if(*no_of_words >= MAX_WORDS)
+  {
+    printf("\nTOO MANY WORDS\n");
+    exit(1);
+  }
+
+  /*if word is not present, add to the structure*/
+  strcpy(words[*no_of_words].s,c);
+  
+  /*since new word,make freq 1*/
+  words[*no_of_words].freq = 1;
+
+  /*increment number of words*/
+  (*no_of_words)++;
+}
+
+/*sort the words in descending order*/
+int word_sort(word_map *first, word_map *second)
+{
+  if(first->freq < second->freq)
+    return +1;
+  
+  if(first->freq > second->freq)
+    return -1;
+}
+
+/*check if alphabets*/
+int check_alphabets(char c)
+{ 
+  if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+/*function to remove the nth character from a string*/
+void remove_character(char *c, int n)
+{
+  while(c[n])
+  {
+    n++;
+    c[n-1] =c[n];
+  }
+  c[n] = 0;
+}
+
+/*remove non alphabetic characters*/
+void remove_non_alphabetic(char *c)
+{
+  int i;
+  for( i = 0; c[i]; i++)
+  {
+    if(check_alphabets(c[i]) == 0)
+    {
+      remove_character(c,i);
+    }
+  }
+}
+
+/*make all letters lowercase*/
+void lowercase_words(char *c)
+{
+  int i;
+  for(i=0; c[i]; i++)
+  {
+    c[i] = tolower(c[i]);
+  }
+}
+
 
 /*Signal handler for SIGINT*/
 void signal_handler()
@@ -87,8 +200,36 @@ static void sigusr1_handler(int sig)
           ++statistics.line_cnt;
         }
       }
+    
+      /*rewind file pointer to start of function*/
+      rewind(fp);
+      char input_word[1000];
+      int i,no_of_words;
 
+      no_of_words = 0;
+      while(fscanf(fp,"%1023s",input_word) == 1)
+      {
+        if(check_alphabets(input_word[0]))
+        {
+          remove_non_alphabetic(input_word);
+          lowercase_words(input_word);
+          word_insert(words,&no_of_words,input_word);
+        }
+      }
+
+     qsort((void *)words, no_of_words ,sizeof(word_map),word_sort);
+
+    if(no_of_words < FREQ_CNT)
+    {
+      statistics.no_top_repeated_words = no_of_words;
     }
+    else
+    {
+      statistics.no_top_repeated_words = FREQ_CNT;
+    }
+
+    
+  }
     
     /*unlock the mutex*/
     pthread_mutex_unlock(&file_lock);
@@ -150,12 +291,17 @@ static void sigusr2_handler(int sig)
     printf("\nLine Count is %d\n",statistics.line_cnt);
     printf("\nword Count is %d\n",statistics.word_cnt);
     printf("\nchar Count is %d\n",statistics.char_cnt);
-
+    
+    printf("\nTOP 5 repeated words are\n");
+    for(int i = 0; i<statistics.no_top_repeated_words; i++)
+    {
+      printf("%s\t%d\n",words[i].s,words[i].freq);
+    }
     /*reset them to zero*/
     statistics.line_cnt = 0;
     statistics.word_cnt = 0;
     statistics.char_cnt = 0;
-
+    statistics.no_top_repeated_words = 0;
     /*unlock the mutex*/
     pthread_mutex_unlock(&count_lock);
 
